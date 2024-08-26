a566db74ef355d53b6f2e580cd3952e7eeea483b
Sagar-Lekhwani <sgrlekhwani@gmail.com>
2024-08-26 21:38:38 +0530

    Db connection and models created

---
+++ models/Restaurant.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const RestaurantSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  location: {
+    type: { type: String, enum: ['Point'], required: true },
+    coordinates: { type: [Number], required: true },
+  },
+  ratings: [Number],
+});
+
+RestaurantSchema.index({ location: '2dsphere' });
+
+module.exports = mongoose.model('Restaurant', RestaurantSchema);
