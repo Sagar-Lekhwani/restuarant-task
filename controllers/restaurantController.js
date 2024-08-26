12edc6d06aaee3043cfb7f04005970ec8a398f7a
Sagar-Lekhwani <sgrlekhwani@gmail.com>
2024-08-27 02:21:44 +0530

    restaurants all controller functions created

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -1,5 +1,7 @@
 const Restaurant = require('../models/Restaurant');
 
+
+// function to create the restaurants
 exports.createRestaurant = async (req, res) => {
   try {
     const { name, description, latitude, longitude, ratings } = req.body;
@@ -16,6 +18,17 @@ exports.createRestaurant = async (req, res) => {
   }
 };
 
+// function to fetch all restaurants 
+exports.getAllRestaurants = async (req, res) => {
+    try {
+      const restaurants = await Restaurant.find();
+      res.json(restaurants);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+
+// function to get restaurant by location   
 exports.getRestaurantsByLocation = async (req, res) => {
   try {
     const { latitude, longitude, radius, minimumDistance, maximumDistance } = req.query;
@@ -34,3 +47,38 @@ exports.getRestaurantsByLocation = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+
+// functtion to update restaurant details 
+exports.UpdateRestaurants = async (req, res) => {
+    try {
+      const restaurant = await Restaurant.findByIdAndUpdate(
+        req.params.id,
+        req.body, 
+        { new: true, runValidators: true }
+      );
+      
+      if (!restaurant) {
+        return res.status(404).json({ message: 'Restaurant not found' });
+      }
+  
+      res.json(restaurant);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  }
+
+// function to delete the restaurant
+exports.DeleteRestaurant = async (req, res) => {
+    try {
+      const restaurant = await Restaurant.findByIdAndDelete(req.params.id);
+      
+      if (!restaurant) {
+        return res.status(404).json({ message: 'Restaurant not found' });
+      }
+  
+      res.json({ message: 'Restaurant deleted successfully' });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  }  
